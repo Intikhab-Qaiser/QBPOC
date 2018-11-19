be1793f0413043c25f3b2a4e13a1f49baa84ee4e
Intikhab-Qaiser <45162863+Intikhab-Qaiser@users.noreply.github.com>
2018-11-19 10:10:59 +0200

    Add files via upload

---
+++ MyCompanyApp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace MyCompanyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.AddCustomer = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(28, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(520, 76);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Note: You need to have QuickBooks with a company file opened.";
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(735, 311);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(312, 57);
+            this.Exit.TabIndex = 19;
+            this.Exit.Text = "Exit";
+            // 
+            // AddCustomer
+            // 
+            this.AddCustomer.Location = new System.Drawing.Point(735, 235);
+            this.AddCustomer.Name = "AddCustomer";
+            this.AddCustomer.Size = new System.Drawing.Size(312, 57);
+            this.AddCustomer.TabIndex = 18;
+            this.AddCustomer.Text = "Add Customer";
+            this.AddCustomer.Click += new System.EventHandler(this.AddCustomer_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(236, 139);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(457, 38);
+            this.txtPhone.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(28, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 39);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Phone";
+            // 
+            // txtCustName
+            // 
+            this.txtCustName.Location = new System.Drawing.Point(236, 82);
+            this.txtCustName.Name = "txtCustName";
+            this.txtCustName.Size = new System.Drawing.Size(811, 38);
+            this.txtCustName.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(28, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 38);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Name";
+            // 
+            // lblError
+            // 
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(28, 538);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(1161, 95);
+            this.lblError.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 658);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.AddCustomer);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCustName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Add Customer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button AddCustomer;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ MyCompanyApp/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using QBFC13Lib;
+using QBXMLRP2Lib;
+
+namespace MyCompanyApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCustomer_Click(object sender, EventArgs e)
+        {
+            var xmlRequestSet = PrepareRequestXml();
+            var xmlResponseSet = SendRequestToQB(xmlRequestSet);
+            var parsedResponseSet = ParseResponseXml(xmlResponseSet);
+            if (!string.IsNullOrEmpty(parsedResponseSet))
+                MessageBox.Show(parsedResponseSet);
+            else
+                MessageBox.Show("No reponse from QuickBook");
+        }
+
+        private string PrepareRequestXml()
+        {
+            XmlDocument inputXMLDoc = new XmlDocument();
+            inputXMLDoc.AppendChild(inputXMLDoc.CreateXmlDeclaration("1.0", null, null));
+            inputXMLDoc.AppendChild(inputXMLDoc.CreateProcessingInstruction("qbxml", "version=\"2.0\""));
+            XmlElement qbXML = inputXMLDoc.CreateElement("QBXML");
+            inputXMLDoc.AppendChild(qbXML);
+            XmlElement qbXMLMsgsRq = inputXMLDoc.CreateElement("QBXMLMsgsRq");
+            qbXML.AppendChild(qbXMLMsgsRq);
+            qbXMLMsgsRq.SetAttribute("onError", "stopOnError");
+            XmlElement custAddRq = inputXMLDoc.CreateElement("CustomerAddRq");
+            qbXMLMsgsRq.AppendChild(custAddRq);
+            custAddRq.SetAttribute("requestID", "1");
+            XmlElement custAdd = inputXMLDoc.CreateElement("CustomerAdd");
+            custAddRq.AppendChild(custAdd);
+            custAdd.AppendChild(inputXMLDoc.CreateElement("Name")).InnerText = txtCustName.Text.Trim();
+            if (txtPhone.Text.Length > 0)
+            {
+                custAdd.AppendChild(inputXMLDoc.CreateElement("Phone")).InnerText = txtPhone.Text.Trim();
+            }
+
+            return inputXMLDoc.OuterXml;
+        }
+
+        private string SendRequestToQB(string xmlRequestSet)
+        {
+            var MyQbXMLRP2 = new RequestProcessor2();
+            var ticket = string.Empty;
+            var responseXml = string.Empty;
+            try
+            {
+                MyQbXMLRP2.OpenConnection2("", "My Sample App", QBXMLRPConnectionType.localQBD);
+                ticket = MyQbXMLRP2.BeginSession("", QBFileMode.qbFileOpenDoNotCare);
+                responseXml = MyQbXMLRP2.ProcessRequest(ticket, xmlRequestSet);
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "Error: " + ex.Message;
+            }
+            finally
+            {
+                MyQbXMLRP2.EndSession(ticket);
+                MyQbXMLRP2.CloseConnection();
+            }
+
+            return responseXml;
+        }
+
+        private string ParseResponseXml(string xmlRepsonseSet)
+        {
+            XmlDocument outputXMLDoc = new XmlDocument();
+            outputXMLDoc.LoadXml(xmlRepsonseSet);
+            XmlNodeList qbXMLMsgsRsNodeList = outputXMLDoc.GetElementsByTagName("CustomerAddRs");
+            StringBuilder popupMessage = new StringBuilder();
+
+            if (qbXMLMsgsRsNodeList.Count == 1) 
+            {
+
+                XmlAttributeCollection rsAttributes = qbXMLMsgsRsNodeList.Item(0).Attributes;
+                string retStatusCode = rsAttributes.GetNamedItem("statusCode").Value;
+                string retStatusSeverity = rsAttributes.GetNamedItem("statusSeverity").Value;
+                string retStatusMessage = rsAttributes.GetNamedItem("statusMessage").Value;
+                popupMessage.AppendFormat("statusCode = {0}, statusSeverity = {1}, statusMessage = {2}",
+                    retStatusCode, retStatusSeverity, retStatusMessage);
+
+                XmlNodeList custAddRsNodeList = qbXMLMsgsRsNodeList.Item(0).ChildNodes;
+                if (custAddRsNodeList.Count == 1 && custAddRsNodeList.Item(0).Name.Equals("CustomerRet"))
+                {
+                    XmlNodeList custRetNodeList = custAddRsNodeList.Item(0).ChildNodes;
+
+                    foreach (XmlNode custRetNode in custRetNodeList)
+                    {
+                        if (custRetNode.Name.Equals("ListID"))
+                        {
+                            popupMessage.AppendFormat("\r\nCustomer ListID = {0}", custRetNode.InnerText);
+                        }
+                        else if (custRetNode.Name.Equals("Name"))
+                        {
+                            popupMessage.AppendFormat("\r\nCustomer Name = {0}", custRetNode.InnerText);
+                        }
+                        else if (custRetNode.Name.Equals("FullName"))
+                        {
+                            popupMessage.AppendFormat("\r\nCustomer FullName = {0}", custRetNode.InnerText);
+                        }
+                    }
+                }
+            }
+
+            return popupMessage.ToString();
+        }
+    }
+}
